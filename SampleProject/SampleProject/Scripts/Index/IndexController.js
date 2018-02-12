654cc332692e3c567fd078a7b17ebdb89ce1cf2a
Andrew <andrewchow002@gmail.com>
2018-02-11 19:49:27 -0800

    Finished sample project

---
+++ SampleProject/SampleProject/Scripts/Index/IndexController.js
@@ -0,0 +1,88 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("mainApp")
+        .controller("IndexController", IndexController);
+
+    IndexController.$inject = ["IndexService"];
+
+    function IndexController(IndexService) {
+        var vm = this;
+        vm.$onInit = _init;
+        vm.IndexService = IndexService;
+        vm.productInfo = [];
+        vm.productList = [];
+        vm.cartCount = 0;
+        vm.cartItems = [];
+        vm.cartTotalPrice = 0;
+
+        vm.sizeFourChunks = _sizeFourChunks;
+        vm.addToCart = _addToCart;
+        vm.removeItem = _removeItem;
+
+        function _init() {
+            //Retrieve products array from service
+            console.log(vm.IndexService.getProductInfo());
+            vm.productInfo = vm.IndexService.getProductInfo();
+            vm.productList = vm.sizeFourChunks(vm.productInfo);
+        }
+
+        function _sizeFourChunks(data) {
+            //Splits array to be easier displayed in rows of 4
+            var chunksize = 4;
+            var chunks = [];
+            data.forEach((item) => {
+                if (!chunks.length || chunks[chunks.length - 1].length == chunksize)
+                    chunks.push([]);
+                chunks[chunks.length - 1].push(item);
+            });
+            return chunks
+        }
+
+        function _addToCart(e) {
+            //Checks if item has already been clicked
+            if ($(e.target).closest(".clothingItem").hasClass("clicked")) {
+                vm.cartCount--;
+                $(e.target).closest(".clothingItem").removeClass("clicked");
+                //Finds the item number, then removes that item from cart
+                var itemNumber = $(e.target).next("input").val();
+                for (var i = 0; i < vm.cartItems.length; i++) {
+                    if (vm.cartItems[i].itemNumber == itemNumber) {
+                        vm.cartTotalPrice -= vm.cartItems[i].price;
+                        vm.cartItems.splice(i, 1);
+                        break
+                    }
+                }
+                //Rounds total to two decimals, included becuase of bug causing long decimals
+                vm.cartTotalPrice = Math.round(vm.cartTotalPrice * 100) / 100;
+            }
+            else {
+                //If item hasn't been clicked, adds item to cart
+                vm.cartCount++;
+                $(e.target).closest(".clothingItem").addClass("clicked");
+                var itemNumber = $(e.target).next("input").val();
+                vm.cartItems.push(vm.productInfo[itemNumber]);
+                vm.cartTotalPrice += vm.productInfo[itemNumber].price;
+                vm.cartTotalPrice = Math.round(vm.cartTotalPrice * 100) / 100;
+            }
+        }
+
+        function _removeItem(e) {
+            //For "x" button in cart modal
+            vm.cartCount--;
+            var itemNumber = $(e.target).next("input").val();
+            $(e.target).closest(".row").remove();
+            //Removes item from cart
+            for (var i = 0; i < vm.cartItems.length; i++) {
+                if (vm.cartItems[i].itemNumber == itemNumber) {
+                    vm.cartTotalPrice -= vm.cartItems[i].price;
+                    vm.cartItems.splice(i, 1);
+                    break
+                }
+            }
+            //Removes "clicked" status on main page of specified item
+            $(".clothingItem input:eq(" + itemNumber + ")").closest(".clicked").removeClass("clicked");
+            vm.cartTotalPrice = Math.round(vm.cartTotalPrice * 100) / 100;
+        }
+    }
+})();
---
+++ SampleProject/SampleProject/Scripts/Index/IndexService.js
@@ -0,0 +1,98 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('mainApp')
+        .factory('IndexService', IndexService);
+
+    IndexService.$inject = ['$http', '$q'];
+
+    function IndexService($http, $q) {
+        var productInfo = [
+            {
+                "filename": "618328744.png",
+                "price": 51.27,
+                "name": "Mens Fashion Shirts",
+                "filelocation": "/Images/618328744.png",
+                "itemNumber" : 0
+            },
+            {
+                "filename": "618842634.png",
+                "price": 57.04,
+                "name": "Casual jacket",
+                "filelocation": "/Images/618842634.png",
+                "itemNumber" : 1
+            },
+            {
+                "filename": "619521178.png",
+                "price": 3.97,
+                "name": "Mens work shirt",
+                "filelocation": "/Images/619521178.png",
+                "itemNumber" : 2
+            },
+            {
+                "filename": "623270836.png",
+                "price": 32.37,
+                "name": "Running Shoes",
+                "filelocation": "/Images/623270836.png",
+                "itemNumber" : 3
+            },
+            {
+                "filename": "638571516.png",
+                "price": 57.74,
+                "name": "Leather lacket",
+                "filelocation": "/Images/638571516.png",
+                "itemNumber" : 4
+            },
+            {
+                "filename": "649597850.png",
+                "price": 87.45,
+                "name": "Snow Boots",
+                "filelocation": "/Images/649597850.png",
+                "itemNumber" : 5
+            },
+            {
+                "filename": "656678076.png",
+                "price": 90.12,
+                "name": "Mens Fashion Shirts",
+                "filelocation": "/Images/656678076.png",
+                "itemNumber" : 6
+            },
+            {
+                "filename": "666666252.png",
+                "price": 88.83,
+                "name": "Womens white sweater",
+                "filelocation": "/Images/666666252.png",
+                "itemNumber" : 7
+            },
+            {
+                "filename": "666666466.png",
+                "price": 77.67,
+                "name": "Womens red jacket",
+                "filelocation": "/Images/666666466.png",
+                "itemNumber" : 8
+            },
+            {
+                "filename": "675606742.png",
+                "price": 70.57,
+                "name": "Pastel everyday wear",
+                "filelocation": "/Images/675606742.png",
+                "itemNumber" : 9
+            },
+            {
+                "filename": "806813490.png",
+                "price": 22.46,
+                "name": "Longsleeve workout shirt",
+                "filelocation": "/Images/806813490.png",
+                "itemNumber" : 10
+            }
+        ]
+        return {
+            getProductInfo: function () {
+                return productInfo;
+            },
+              setProductInfo: function (value) {
+                productInfo=value;
+            }
+        };
+    }
+})();
